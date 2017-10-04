9e5236e34ca328a4aa57eaf2866af5f4f44e845d
Caroline Pereira Hoegen <carolhoegen@hotmail.com>
2017-10-04 16:03:19 -0300

    Alter object and database structure

--- ClientsArea.Entities/Client.cs
+++ ClientsArea.Entities/Client.cs
@@ -8,8 +8,8 @@ namespace ClientsArea.Entities
         public string Name { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
-        public string Latitude { get; set; }
-        public string Longitude { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
         public string Address { get; set; }
     }
 }
--- Database/CREATE TABLE client.sql
+++ Database/CREATE TABLE client.sql
@@ -3,9 +3,9 @@ CREATE EXTENSION "uuid-ossp";
 CREATE TABLE client (
     clientId uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
     name varchar(250) null, 
-	address varchar(250) null, 
+    address varchar(250) null, 
     email varchar(250) null, 
     phone varchar(20) null, 
-    latitude varchar (50) not null, 
-    longitude varchar(50) not null
+    latitude int not null, 
+    longitude int not null
 );
